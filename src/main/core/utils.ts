a47616aabd0d14588f292fa8e2749b1f2baf7077
Klaus Reimer <k@ailis.de>
2021-10-02 09:57:57 +0200

    Cleanup copyrights

--- src/main/core/utils.ts
+++ src/main/core/utils.ts
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2021 Klaus Reimer <k@ailis.de>
- * Copyright (C) 2014-present, Facebook, Inc.
+ * Copyright (C) 2014 Facebook, Inc. and its affiliates
  *
  * See LICENSE.md for licensing information.
  */
