3b033472b9690e698e03780dea5140ef40b677ff
Klaus Reimer <k@ailis.de>
2021-10-02 09:10:33 +0200

    Start restructuring project

--- __main_process_tests__/main_process_4.test.js
+++
@@ -1,3 +0,0 @@
-test('main process', () => {
-  expect(global.window).not.toBeDefined();
-});
--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-flow'],
-  plugins: [
-    ['transform-es2015-modules-commonjs', {allowTopLevelThis: true}],
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-  ],
-  retainLines: true,
-};
